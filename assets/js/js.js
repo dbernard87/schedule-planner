ea3e6044b07dc6075e85ea546171b05246cc60a5
David Bernard <dbernardcoding@gmail.com>
2021-04-05 22:30:55 -0400

    adding variables and functions for all standard business hours

--- assets/js/js.js
+++ assets/js/js.js
@@ -30,3 +30,108 @@ function writeTaskTen() {
 	localStorage.setItem('taskTen', inputTen.value);
 	outputTen.textContent = inputTen.value;
 }
+
+/*---------- 11:00 AM ----------*/
+let inputEleven = document.querySelector('.writeTaskEleven');
+let outputEleven = document.querySelector('.writeTaskEleven');
+let saveEleven= document.querySelector('#clipEleven');
+/*---------- ----------*/
+saveEleven.addEventListener('click', writeTaskEleven);
+/*---------- ----------*/
+outputEleven.textContent = localStorage.getItem('taskEleven');
+inputEleven.value = localStorage.getItem('taskEleven');
+/*---------- ----------*/
+function writeTaskEleven() {
+	localStorage.setItem('taskEleven', inputEleven.value);
+	outputEleven.textContent = inputEleven.value;
+}
+
+/*---------- 12:00 PM ----------*/
+let inputTwelve = document.querySelector('.writeTaskTwelve');
+let outputTwelve = document.querySelector('.writeTaskTwelve');
+let saveTwelve= document.querySelector('#clipTwelve');
+/*---------- ----------*/
+saveTwelve.addEventListener('click', writeTaskTwelve);
+/*---------- ----------*/
+outputTwelve.textContent = localStorage.getItem('taskTwelve');
+inputTwelve.value = localStorage.getItem('taskTwelve');
+/*---------- ----------*/
+function writeTaskTwelve() {
+	localStorage.setItem('taskTwelve', inputTwelve.value);
+	outputTwelve.textContent = inputTwelve.value;
+}
+
+/*---------- 1:00 PM ----------*/
+let inputOne = document.querySelector('.writeTaskOne');
+let outputOne = document.querySelector('.writeTaskOne');
+let saveOne= document.querySelector('#clipOne');
+/*---------- ----------*/
+saveOne.addEventListener('click', writeTaskOne);
+/*---------- ----------*/
+outputOne.textContent = localStorage.getItem('taskOne');
+inputOne.value = localStorage.getItem('taskOne');
+/*---------- ----------*/
+function writeTaskOne() {
+	localStorage.setItem('taskOne', inputOne.value);
+	outputOne.textContent = inputOne.value;
+}
+
+/*---------- 2:00 PM ----------*/
+let inputTwo = document.querySelector('.writeTaskTwo');
+let outputTwo = document.querySelector('.writeTaskTwo');
+let saveTwo= document.querySelector('#clipTwo');
+/*---------- ----------*/
+saveTwo.addEventListener('click', writeTaskTwo);
+/*---------- ----------*/
+outputTwo.textContent = localStorage.getItem('taskTwo');
+inputTwo.value = localStorage.getItem('taskTwo');
+/*---------- ----------*/
+function writeTaskTwo() {
+	localStorage.setItem('taskTwo', inputTwo.value);
+	outputTwo.textContent = inputTwo.value;
+}
+
+/*---------- 3:00 PM ----------*/
+let inputThree = document.querySelector('.writeTaskThree');
+let outputThree = document.querySelector('.writeTaskThree');
+let saveThree= document.querySelector('#clipThree');
+/*---------- ----------*/
+saveThree.addEventListener('click', writeTaskThree);
+/*---------- ----------*/
+outputThree.textContent = localStorage.getItem('taskThree');
+inputThree.value = localStorage.getItem('taskThree');
+/*---------- ----------*/
+function writeTaskThree() {
+	localStorage.setItem('taskThree', inputThree.value);
+	outputThree.textContent = inputThree.value;
+}
+
+/*---------- 4:00 PM ----------*/
+let inputFour = document.querySelector('.writeTaskFour');
+let outputFour = document.querySelector('.writeTaskFour');
+let saveFour= document.querySelector('#clipFour');
+/*---------- ----------*/
+saveFour.addEventListener('click', writeTaskFour);
+/*---------- ----------*/
+outputFour.textContent = localStorage.getItem('taskFour');
+inputFour.value = localStorage.getItem('taskFour');
+/*---------- ----------*/
+function writeTaskFour() {
+	localStorage.setItem('taskFour', inputFour.value);
+	outputFour.textContent = inputFour.value;
+}
+
+/*---------- 4:00 PM ----------*/
+let inputFive = document.querySelector('.writeTaskFive');
+let outputFive = document.querySelector('.writeTaskFive');
+let saveFive= document.querySelector('#clipFive');
+/*---------- ----------*/
+saveFive.addEventListener('click', writeTaskFive);
+/*---------- ----------*/
+outputFive.textContent = localStorage.getItem('taskFive');
+inputFive.value = localStorage.getItem('taskFive');
+/*---------- ----------*/
+function writeTaskFive() {
+	localStorage.setItem('taskFive', inputFive.value);
+	outputFive.textContent = inputFive.value;
+}
